cc75779e1f56ab06d159462077261da4f5125934
Esteni Munguia <esteni.m.munguia@gmail.com>
2017-03-10 16:41:53 -0800

    added crud project

---
+++ rg_demo/rg_demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(rg_demo.Startup))]
+namespace rg_demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
