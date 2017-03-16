f223c1450d0686c653cec01b16709e25db98bd64
est-melissa <esteni.m.munguia@gmail.com>
2017-03-16 11:31:02 -0700

    Update AssemblyInfo.cs

--- rg_demo/rg_demo/Properties/AssemblyInfo.cs
+++ rg_demo/rg_demo/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("98c8043a-8871-4f13-9ac5-0dc2441d2b25")]
+[assembly: Guid("")]
 
 // Version information for an assembly consists of the following four values:
 //
